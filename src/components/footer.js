8310d0410347f874e19c0f13c8ba204aed116c4b
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-20 19:42:35 -0400

    main | Fix compiler warnings.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,21 @@ import * as React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <a href='https://www.facebook.com/carlie.amore' target='_blank' className={'fa fa-facebook'}></a>
-      <a href='https://twitter.com/hopinmycarl' target='_blank' className={'fa fa-twitter'}></a>
-      <a href='https://www.instagram.com/' target='_blank' className={'fa fa-instagram'}></a>
-      <a href='https://www.snapchat.com/' target='_blank' className={'fa fa-snapchat'}></a>
-      <a href='https://www.linkedin.com/' target='_blank' className={'fa fa-linkedin'}></a>
+      <a href='https://www.facebook.com/carlie.amore' target='_blank' rel='noreferrer' aria-label="Facebook link to Carlie Amore\'s profile page">
+        <i className={'fa fa-facebook'}></i>
+      </a>
+      <a href='https://twitter.com/hopinmycarl' target='_blank' rel='noreferrer' aria-label="Twitter link to Carlie Amore\'s profile page">
+        <i className={'fa fa-twitter'}></i>
+      </a>
+      <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' aria-label="Instagram link to Carlie Amore\'s profile page">
+        <i className={'fa fa-instagram'}></i>
+      </a>
+      <a href='https://www.snapchat.com/' target='_blank' rel='noreferrer' aria-label="Snapchat link to Carlie Amore\'s profile page">
+        <i className={'fa fa-snapchat'}></i>
+      </a>
+      <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer' aria-label="LinkedIn link to Carlie Amore\'s profile page">
+        <i className={'fa fa-linkedin'}></i>
+      </a>
     </footer>
   )
 }
