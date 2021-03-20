7150a9c1943dfa195f6c82187e3bc4849907f501
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-20 19:47:19 -0400

    main | Fix compiler warnings.

--- src/pages/contact-me.js
+++ src/pages/contact-me.js
@@ -7,12 +7,15 @@ const ContactMePage = () => {
       <h1>Contact Me</h1>
       <p>If you have any questions or would like more information, please fill out the form below and I'll get back to you as soon as possible.</p>
       <form>
-        <label for='firstName'>First name:</label>
-        <input type='text' name='firstName'></input>
-        <label for='emailAddress'>Email address:</label>
-        <input type='email' name='emailAddress'></input>
-        <label for='comments'>Comment:</label>
-        <textarea name='comments'></textarea>
+        <label for='firstName'>First name:
+          <input type='text' name='firstName' id='firstName'></input>
+        </label>
+        <label for='emailAddress'>Email address:
+          <input type='email' name='emailAddress' id='emailAddress'></input>
+        </label>
+        <label for='comments'>Comment:
+          <textarea name='comments' id='comments'></textarea>
+        </label>
         <input type='submit' value='Submit' />
       </form>
     </PageLayout>
