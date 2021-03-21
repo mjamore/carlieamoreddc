04151d5c370c8a59eafa4de33d6fa9e267ec6b4e
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-21 13:33:12 -0400

    main | Add hamburger menu library. Move styles into separate files.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { slide as Menu } from 'react-burger-menu';
 import * as styles from './navigation.module.css';
 
 const Navigation = () => {
   return (
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/" className={styles.navigationItemLink}>Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/about-me/" className={styles.navigationItemLink}>About Me</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/services/" className={styles.navigationItemLink}>Services</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/broadmoor-holistic-dentistry/" className={styles.navigationItemLink}>Broadmoor Holistic Dentistry</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/contact-me/" className={styles.navigationItemLink}>Contact Me</Link>
-      </li>
-    </ul>
+    <Menu>
+      <ul className={styles.navigation}>
+        <li className={styles.navigationItem}>
+          <Link to="/" className={styles.navigationItemLink}>Home</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/about-me/" className={styles.navigationItemLink}>About Me</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/services/" className={styles.navigationItemLink}>Services</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/broadmoor-holistic-dentistry/" className={styles.navigationItemLink}>Broadmoor Holistic Dentistry</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/contact-me/" className={styles.navigationItemLink}>Contact Me</Link>
+        </li>
+      </ul>
+    </Menu>
   )
 }
 
