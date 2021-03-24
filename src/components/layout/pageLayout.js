1b712208904be13a90663e241a61262fe4dce45d
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-24 02:42:34 -0400

    main | Add 'media' attribute to font awesome css link in head section.

--- src/components/layout/pageLayout.js
+++ src/components/layout/pageLayout.js
@@ -7,7 +7,7 @@ const PageLayout = ({ children }) => {
   return (
     <div>
       <Helmet>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' />
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' media='all' />
         <html lang='en' />
       </Helmet>
       <Header />
