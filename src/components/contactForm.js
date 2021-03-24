93cd6b915e2da1c96da40c4e94777c655ec221de
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-24 01:54:54 -0400

    main | Add validation to contact form.

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,20 +1,36 @@
 import * as React from 'react';
+import { useForm } from 'react-hook-form';
 import * as styles from './contactForm.module.css';
 
+const submitForm = (formData) => {
+  console.log('formData: ', formData);
+};
+
+const RequiredFieldErrorMessage = () => {
+  return(
+    <span className={styles.formError}>This field is required</span>
+  );
+};
+
 const ContactForm = () => {
+  const { register, handleSubmit, errors } = useForm();
+
   return (
-    <form className={styles.form}>
-      <label className={styles.label} for='name'>Name:
-        <input className={styles.input} type='text' name='name' id='name'></input>
+    <form className={styles.form} onSubmit={handleSubmit(submitForm)}>
+      <label className={styles.label} htmlFor='name'>Name <span className={styles.requiredAsterisk}>*</span>
+        <input className={styles.input} type='text' name='name' id='name' ref={register({ required: true })} />
+        {errors.name && <RequiredFieldErrorMessage />}
       </label>
-      <label className={styles.label} for='emailAddress'>Email address:
-        <input className={styles.input} type='email' name='emailAddress' id='emailAddress'></input>
+      <label className={styles.label} htmlFor='emailAddress'>Email address <span className={styles.requiredAsterisk}>*</span>
+        <input className={styles.input} type='email' name='emailAddress' id='emailAddress' ref={register({ required: true })} />
+        {errors.emailAddress && <RequiredFieldErrorMessage />}
       </label>
-      <label className={styles.label} for='phone'>Phone Number:
-        <input className={styles.input} type='number' name='phone' id='phone'></input>
+      <label className={styles.label} htmlFor='phone'>Phone Number
+        <input className={styles.input} type='number' name='phone' id='phone' ref={register} />
       </label>
-      <label className={styles.label} for='comments'>Comment:
-        <textarea className={styles.textarea} name='comments' id='comments'></textarea>
+      <label className={styles.label} htmlFor='message'>Message <span className={styles.requiredAsterisk}>*</span>
+        <textarea className={styles.textarea} name='message' id='message' ref={register({ required: true })} />
+        {errors.message && <RequiredFieldErrorMessage />}
       </label>
       <input type='submit' value='Submit' />
     </form>
