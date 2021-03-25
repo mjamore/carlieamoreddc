cd306b5134ae5ec6b0d860cb45fdc4a2ba9e7838
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-25 03:19:54 -0400

    main | Fix eslint errors.

---
+++ src/data/servicesProvided.js
@@ -0,0 +1,52 @@
+const servicesProvidedData = [
+  {
+    name: 'Cleanings',
+    description: 'We recommend professional cleanings at varying intervals. When the gums are free of disease, 1-2x per year is appropriate. However, some people benefit from cleanings more often. Excellent home care will reduce the need for more visits, although genetics, epigenetic, bacterial load and other factors also play a role. At your first visit, we will discover the health of your gums and determine what type of cleaning you will need. Our hygienists are excellent at removing soft and hard deposits from above and below the gum line. Many practices only allow 30-40 minutes per hygiene appointment. We schedule an entire hour to allow ample time to be thorough with extra time for relationship building and patient education.',
+  },
+  {
+    name: 'Safe Mercury Removal',
+    description: 'Dr. Babcock and Dr. Amore are both SMART certified by the IAOMT to remove mercury fillings and follow the recommended protocol to safely do so. We take this procedure very seriously and will protect you from the vapor and toxic debris.  Many people do not realize that silver or amalgam fillings are 50% elemental mercury, the most toxic non-radioactive element on earth.  A large filling may contain the same amount of mercury as a thermometer or more.  When thermometers break, even large buildings like high schools are evacuated until a HAZMAT team is called in to clean up. No amount of mercury is safe inside or outside your mouth. Check out our resource page for more information on the possible deleterious effects.',
+  },
+  {
+    name: 'Deep Cleanings',
+    description: 'Sometimes when we first meet a patient, the gums are infected. Perhaps it had been a long time since the last cleaning or their previous cleanings may have been ineffective or inadequate. When there is infection present, such as bleeding, deep pocketing, swelling, redness, and bone loss, the bacteria is much deeper below the gum line and likely more pathogenic in nature. In these cases special instruments, anesthetic, increased care, skill, judgment, and time are required. This process is called scaling and root planing.  Once the disease is under control and the patient is committed to excellent home care, restorative treatment can begin and it can be expected to last.',
+  },
+  {
+    name: 'Biorejuvenation Dentistry for TMJ',
+    description: 'Dr. Babcock is extensively trained in the diagnosis and treatment of TMJ and bite problems.  Rejuvenation dentistry asks the question “why” is there enamel wear, fractures, chipping, repeated broken teeth and gum recession.  Dr. Babcock understands that these symptoms along with popping and clicking, headaches, facial pain, neck, shoulder, back, and even arm pain,  are a result of a chewing system that is out of balance.  The entire system must be addressed, not just symptom treatment like placing crowns on broken teeth. Why did the tooth break in the first place, and how many more will break if the root cause is ignored? As humans we are beautifully designed to function with sharp cusp tips and minimal to no wear for a lifetime.  Breakage and chipping is not a normal part of the aging process!',
+  },
+  {
+    name: 'Periodontal Maintenance',
+    description: 'Periodontal maintenance is performed when there is a history of gum disease or there are current areas of disease. These cleanings allow the hygienist to go further below the gum line to remove hard and soft deposits. Most patients receiving periodontal maintenance will be seen every 3 months.',
+  },
+  {
+    name: 'Broken Veneers, Crowns & Teeth',
+    description: 'Frustration with repeatedly broken veneers, crowns, and teeth is common.  If a dentist places veneers to cover or hide worn or broken teeth without first addressing the cause of fractures in the first place, the problems will continue.  Instead of broken or worn teeth, the veneers may keep coming off or break.  If they don’t break, the opposing teeth without porcelain may experience rapid wear because enamel and dentin are softer than porcelain.  To put it simply, covering the problem will not fix it long term, it will only hide the symptoms until it comes out somewhere else.',
+  },
+  {
+    name: 'Cosmetic Dentistry',
+    description: 'Cosmetic dentistry is done to improve the appearance of the smile. Porcelain veneers are a very thin covering placed over the teeth and can turn stained, crooked, or otherwise unattractive teeth into a beautiful perfect smile in just 2 visits. The artistic ability of the dentist and laboratory technician together as a team are critical for success. Although there are many cheaper options, Dr. Babcock chooses to work with a local lab that has outstanding ceramists. At times we send the patient to the lab for undetectable color matching. Their work is beautiful, and most importantly natural looking. When the ceramist uses techniques to mimic natural enamel the result is beautiful. Success is achieved when nobody can tell you have porcelain and it looks like a gorgeous smile you were born with!',
+  },
+  {
+    name: 'Minimally Invasive Dentistry',
+    description: 'All Rejuvenation Dentistry is done in the least invasive way possible. When the chewing system (bite) is in balance, there is no need to remove enamel unnecessarily.  Many dentists treating TMJ or bite problems will grind all of the teeth down and place crowns.  This is not only irreversibly destructive and expensive, it is unnecessary when the joint and bite are properly balanced.  As humans, we are designed to function with beautiful, natural enamel, not porcelain. This is why we use a softer material more similar to enamel, like composite when possible to restore the teeth to a balanced bite.',
+  },
+  {
+    name: 'Teeth Whitening',
+    description: 'Teeth whitening is an easy way to improve your smile. It is a simple process that can be done at home with custom made trays. It takes about 2 weeks, 30 minutes a day to give an excellent result. These trays can be stored and used for touch up indefinitely. We also provide two different types of whitening gel material for patients to choose from based on the results they are looking for while staying holistically minded.',
+  },
+  {
+    name: 'Pediatric Dentistry & Orthodontics',
+    description: 'Dr. Amore enjoys working with kids and provides care to many families. We are proficient at doing conservative treatments on children with active disease. We also are very passionate about early childhood intervention in a natural way that supports kids\' growth and development. We are trained HealthyStart® providers to help address and treat sleep and breathing disorders in children of all ages. The HealthyStart® System is a non-invasive, non-pharmaceutical, pain-free approach to opening the airway and straightening teeth WITHOUT braces. HealthyStart® is rooted in 54 years of research and experience and has treated over 4 million children around the world.',
+  },
+  {
+    name: 'Myofunctional Therapy',
+    description: 'Teeth whitening is an easy way to improve your smile. It is a simple process that can be done at home with custom made trays. It takes about 2 weeks, 30 minutes a day to give an excellent result. These trays can be stored and used for touch up indefinitely. We also provide two different types of whitening gel material for patients to choose from based on the results they are looking for while staying holistically minded.',
+  },
+  {
+    name: 'Frenuloplasty',
+    description: 'A procedure to correct tongue tie (ankyglossia) by using sutures to lengthen the lingual frenulum for patients with restricted tongue range of motion.',
+  },
+];
+
+export default servicesProvidedData;
