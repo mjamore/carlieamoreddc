f9fe585fb6abc649554b6d3be5aaadb9ff99d5e8
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-25 01:59:44 -0400

    main | Fix eslint errors.

--- src/components/layout/footerNavigation.js
+++ src/components/layout/footerNavigation.js
@@ -4,20 +4,22 @@ import navigationLinks from '../../data/navigationLinks';
 import * as styles from './footerNavigation.module.css';
 
 const FooterNavigation = () => {
-
-  const navigationItems = navigationLinks.map((navigationItem, index) => {
-    return (
-      <li key={index}>
-        <Link to={navigationItem.href} className={styles.navigationItemLink}>{navigationItem.text}</Link>
-      </li>
-    );
-  });
+  const navigationItems = navigationLinks.map((navigationItem) => (
+    <li key={navigationItem.href}>
+      <Link
+        to={navigationItem.href}
+        className={styles.navigationItemLink}
+      >
+        {navigationItem.text}
+      </Link>
+    </li>
+  ));
 
   return (
-    <ul className={styles.navigation}>
+    <ul>
       {navigationItems}
     </ul>
-  )
-}
+  );
+};
 
 export default FooterNavigation;
