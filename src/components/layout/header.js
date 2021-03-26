50faed7714dd4f1d1eb2caef14bf63638f4cf921
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-26 04:09:50 -0400

    main | Remove headerNavigation from header.js file.

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import Logo from './logo';
-import HeaderNavigation from './headerNavigation';
 import { header } from './header.module.css';
 
 const Header = () => (
   <header className={header}>
     <Logo />
-    {/* <HeaderNavigation /> */}
   </header>
 );
 
