b4fc3243539367dbce1e5ebbc7064368df9b6b7f
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-26 11:40:44 -0400

    main | Configure gatsby-source-filesystem.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,47 @@
 module.exports = {
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-offline`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Dr. Carlie Amore, DDS`,
-                short_name: `Dr. Carlie Amore, DDS`,
-                start_url: `/`,
-                background_color: `#f7f0eb`,
-                theme_color: `#a2466c`,
-                display: `standalone`,
-                cache_busting_mode: 'none',
-                prefer_related_applications: false,
-                icon: `src/images/favicons/favicon.png`,
-                icons: [
-                    {
-                      src: `/src/images/favicons/android-chrome-192x192.png`,
-                      sizes: `192x192`,
-                      type: `image/png`,
-                      purpose: `any maskable`
-                    },
-                    {
-                      src: `/src/images/favicons/android-chrome-512x512.png`,
-                      sizes: `512x512`,
-                      type: `image/png`,
-                    },
-                    {
-                        src: `/src/images/favicons/apple-touch-icon.png`,
-                        sizes: `180x180`,
-                        type: `image/png`,
-                    },
-                ]
-            },
-        }
-    ]
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Dr. Carlie Amore, DDS',
+        short_name: 'Dr. Carlie Amore, DDS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        cache_busting_mode: 'none',
+        prefer_related_applications: false,
+        icon: 'src/images/favicons/favicon.png',
+        icons: [
+          {
+            src: '/src/images/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/src/images/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/src/images/favicons/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
+  ],
+};
