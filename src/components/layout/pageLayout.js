bf38ee44ff9e248e19f2c79ddf5e80b0d607dced
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-27 13:23:21 -0400

    main | Disable horizontal scroll on mobile.

--- src/components/layout/pageLayout.js
+++ src/components/layout/pageLayout.js
@@ -10,6 +10,7 @@ const PageLayout = ({ children }) => (
     <Helmet>
       <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' media='all' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
       <html lang='en' />
     </Helmet>
     <HeaderNavigation />
