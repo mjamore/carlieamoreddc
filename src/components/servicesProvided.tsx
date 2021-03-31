ef4de46c0bf8c2147b9643c0619fe551463a8953
ddcmichaela <michael.amore@coxautoinc.com>
2021-03-31 05:38:38 -0400

    main | Convert servicesProvided component to typescript.

--- src/components/servicesProvided.tsx
+++ src/components/servicesProvided.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import servicesProvidedData from '../data/servicesProvided';
 
-const ServicesProvided = () => {
-  const services = servicesProvidedData.map((service) => (
+// Typescript declarations
+interface Service {
+  name: string;
+  description: string;
+}
+
+const ServicesProvided = (): ReactElement => {
+  const services = servicesProvidedData.map((service: Service): ReactElement => (
     <div key={service.name} className='bg-white border border-gray-200 m-5 max-w-sm'>
       <div className='bg-aliceblue pt-5 text-center'>
         <h3 className='mb-5 text-lg'>{service.name}</h3>
