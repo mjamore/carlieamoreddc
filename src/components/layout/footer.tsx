cebfea68cf0610841cfcb507392b59c3c3b66dc9
ddcmichaela <michael.amore@coxautoinc.com>
2021-04-01 17:10:48 -0400

    main | Adjust footer alignment.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,27 +3,29 @@ import FooterNavigation from './footerNavigation';
 import ContactForm from '../forms/contactForm';
 
 const Footer = (): ReactElement => (
-  <footer className='bg-aliceblue flex flex-col items-center justify-around py-12 px-5 shadow-container text-center md:flex-row md:items-center'>
-    <FooterNavigation />
-    <div className='my-12 order-3 md:my-0 md:order-none md:self-end'>
-      <a href='https://www.facebook.com/carlie.amore' target='_blank' rel='noreferrer' aria-label="Facebook link to Carlie Amore\'s profile page">
-        <i className='fa fa-facebook' />
-      </a>
-      <a href='https://twitter.com/hopinmycarl' target='_blank' rel='noreferrer' aria-label="Twitter link to Carlie Amore\'s profile page">
-        <i className='fa fa-twitter' />
-      </a>
-      <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' aria-label="Instagram link to Carlie Amore\'s profile page">
-        <i className='fa fa-instagram' />
-      </a>
-      <a href='https://www.snapchat.com/' target='_blank' rel='noreferrer' aria-label="Snapchat link to Carlie Amore\'s profile page">
-        <i className='fa fa-snapchat' />
-      </a>
-      <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer' aria-label="LinkedIn link to Carlie Amore\'s profile page">
-        <i className='fa fa-linkedin' />
-      </a>
-      <p className='mt-5'>© 2021 Carlie Amore, DDS | All Rights Reserved.</p>
+  <footer className='bg-aliceblue py-12 px-5 shadow-container text-center'>
+    <div className='flex flex-col items-center justify-between max-w-page mx-auto md:flex-row md:items-center'>
+      <FooterNavigation />
+      <div className='my-12 order-3 md:my-0 md:order-none md:self-end'>
+        <a href='https://www.facebook.com/carlie.amore' target='_blank' rel='noreferrer' aria-label="Facebook link to Carlie Amore\'s profile page">
+          <i className='fa fa-facebook' />
+        </a>
+        <a href='https://twitter.com/hopinmycarl' target='_blank' rel='noreferrer' aria-label="Twitter link to Carlie Amore\'s profile page">
+          <i className='fa fa-twitter' />
+        </a>
+        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer' aria-label="Instagram link to Carlie Amore\'s profile page">
+          <i className='fa fa-instagram' />
+        </a>
+        <a href='https://www.snapchat.com/' target='_blank' rel='noreferrer' aria-label="Snapchat link to Carlie Amore\'s profile page">
+          <i className='fa fa-snapchat' />
+        </a>
+        <a href='https://www.linkedin.com/' target='_blank' rel='noreferrer' aria-label="LinkedIn link to Carlie Amore\'s profile page">
+          <i className='fa fa-linkedin' />
+        </a>
+        <p className='mt-5'>© 2021 Carlie Amore, DDS | All Rights Reserved.</p>
+      </div>
+      <ContactForm />
     </div>
-    <ContactForm />
   </footer>
 );
 
