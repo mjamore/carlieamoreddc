1e57e6619024a5bb9b1d654cf7313ab2fd5c996a
ddcmichaela <michael.amore@coxautoinc.com>
2021-04-05 23:09:34 -0400

    main | Add navigation Links data file for reference.

---
+++ src/data/navgiationLinks.js
@@ -0,0 +1,58 @@
+const navigationLinks = [
+  {
+    text: 'Home',
+    href: '/',
+  },
+  {
+    text: 'My Education',
+    href: '/my-education/',
+  },
+  {
+    text: 'Specialties',
+    href: '/specialties/',
+    children: [
+      {
+        text: 'Ozone Therapy',
+        href: '/specialties/ozone-therapy/',
+      },
+      {
+        text: 'Safe Mercury Removal',
+        href: '/specialties/safe-mercury-removal/',
+      },
+      {
+        text: 'Pediatric Dentistry',
+        href: '/specialties/pediatric-dentistry/',
+      },
+      {
+        text: 'Airway and Tongue Tie',
+        href: '/specialties/airway-and-tongue-tie/',
+      },
+      {
+        text: 'General Restorative Dentistry',
+        href: '/specialties/general-restorative-dentistry/',
+      },
+      {
+        text: 'Extractions and Bone Grafts',
+        href: '/specialties/extractions-and-bone-grafts/',
+      },
+    ],
+  },
+  {
+    text: 'Holistic Dentistry',
+    href: '/holistic-dentistry/',
+  },
+  {
+    text: 'Frequently Asked Questions',
+    href: '/frequently-asked-questions/',
+  },
+  {
+    text: 'Broadmoor Holistic Dentistry',
+    href: '/broadmoor-holistic-dentistry/',
+  },
+  {
+    text: 'Patient Testimonials',
+    href: '/patient-testimonials/',
+  },
+];
+
+export default navigationLinks;
