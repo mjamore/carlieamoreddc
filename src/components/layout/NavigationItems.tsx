315ad8fc8418489bf04024f188933f949ba978da
ddcmichaela <michael.amore@coxautoinc.com>
2021-04-07 21:23:00 -0400

    main | Add links to specialty pages.

--- src/components/layout/NavigationItems.tsx
+++ src/components/layout/NavigationItems.tsx
@@ -1,40 +1,27 @@
 import React, { ReactElement } from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
-
-// Typescript declarations
-interface NavigationLink {
-  node: {
-    href: string;
-    text: string;
-    order: number;
-  }
-}
+import { Link } from 'gatsby';
 
 const NavigationItem = (): ReactElement => {
-  const data = useStaticQuery(graphql`
-    query navigationLinksHeader {
-      allContentfulNavigationLinks(sort: { fields: order, order: ASC }) {
-        edges {
-          node {
-            href
-            text
-            order
-          }
-        }
-      }
-    }
-  `);
+  const linkClasses = 'block hover:text-black no-underline pr-3 py-3 text-base text-font-color text-left w-full';
 
   return (
-    data.allContentfulNavigationLinks.edges.map(({ node }: NavigationLink): ReactElement => (
-      <Link
-        key={node.href}
-        to={node.href}
-        className='block hover:text-black no-underline pr-3 py-3 text-base text-font-color text-left w-full'
-      >
-        {node.text}
-      </Link>
-    ))
+    <div>
+      <Link to='//' className={linkClasses}>Home</Link>
+      <Link to='/my-education/' className={linkClasses}>My Education</Link>
+      <Link to='/specialties/' className={linkClasses}>Specialty Services</Link>
+      <div className='ml-7'>
+        <Link to='/specialties/ozone-therapy' className={linkClasses}>Ozone Therapy</Link>
+        <Link to='/specialties/pediatric-dentistry' className={linkClasses}>Pediatric Dentistry</Link>
+        <Link to='/specialties/safe-mercury-removal' className={linkClasses}>Safe Mercury Removal</Link>
+        <Link to='/specialties/general-restorative-dentistry' className={linkClasses}>General Restorative Dentistry</Link>
+        <Link to='/specialties/airway-and-tongue-tie' className={linkClasses}>Airway and Tongue Tie</Link>
+        <Link to='/specialties/extractions-and-bone-grafts' className={linkClasses}>Extractions and Bone Grafts</Link>
+      </div>
+      <Link to='/holistic-dentistry/' className={linkClasses}>All About Holisitic Dentistry</Link>
+      <Link to='/frequently-asked-questions/' className={linkClasses}>Frequently Asked Questions</Link>
+      <Link to='/broadmoor-holistic-dentistry/' className={linkClasses}>Broadmoor Holistic Dentistry</Link>
+      <Link to='/patient-testimonials/' className={linkClasses}>Patient Testimonials</Link>
+    </div>
   );
 };
 
--- src/pages/specialties/index.tsx
+++ src/pages/specialties/index.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 import { Helmet } from 'react-helmet';
+import { Link } from 'gatsby';
 import PageLayout from '../../components/layout/PageLayout';
-import ServicesProvided from '../../components/ServicesProvided';
 import PageHeading from '../../components/PageHeading';
 
 const ServicesPages = (): ReactElement => (
@@ -16,7 +16,12 @@ const ServicesPages = (): ReactElement => (
     <PageLayout>
       <div className='m-12'>
         <PageHeading headingText='Specialty Services' />
-        <ServicesProvided />
+        <Link to='/specialties/ozone-therapy/' className='block link'>Ozone Therapy</Link>
+        <Link to='/specialties/pediatric-dentistry/' className='block link'>Pediatric Dentistry</Link>
+        <Link to='/specialties/safe-mercury-removal/' className='block link'>Safe Mercury Removal</Link>
+        <Link to='/specialties/general-restorative-dentistry/' className='block link'>General Restorative Dentistry</Link>
+        <Link to='/specialties/airway-and-tongue-tie/' className='block link'>Airway and Tongue Tie</Link>
+        <Link to='/specialties/extractions-and-bone-grafts/' className='block link'>Extractions and Bone Grafts</Link>
       </div>
     </PageLayout>
   </div>
